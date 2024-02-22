136a7f525259579a0099c89ae63f7b10374827f3
Benjamin DalFavero <bdalfavero@gmail.com>
2024-02-22 12:09:48 -0500

    Added skeleton of main loop.

--- main.c
+++ main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <mpi.h>
+
+#define MSG_SIZE 1024
+#define ITER 100
 
 int main(int argc, char *argv[]) {
 
@@ -9,7 +13,24 @@ int main(int argc, char *argv[]) {
     int tasks;
     MPI_Comm_size(MPI_COMM_WORLD, &tasks);
 
-    printf("Hello, world\n");
+    char *msg = malloc(MSG_SIZE);
+
+    int partner_rank;
+    if (rank % 2 == 0) {
+        partner_rank = (rank + 1) % tasks;
+    } else {
+        partner_rank = (rank - 1) % tasks;
+    }
+
+    for (int i = 0; i < ITER; i++) {
+        if ((rank % 2 == 0 && i % 2 == 0) || (rank % 2 != 0 && i % 2 != 0)) {
+            /* Send the message. */
+        } else {
+            /* Receive a message. */
+        }
+    }
+
+    free(msg);
 
     MPI_Finalize();
 
