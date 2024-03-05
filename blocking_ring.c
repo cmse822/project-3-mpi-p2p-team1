761c6a4d626fbc13e218840bc7149da312b9a1b0
Reza <reza@gamertag.app>
2024-03-05 01:37:04 -0500

    an experimental blocking ring implementation

--- blocking_ring.c
+++ blocking_ring.c
@@ -2,34 +2,51 @@
 #include <stdlib.h>
 #include <mpi.h>
 
+#define ITER 100 // Define the number of iterations for statistical significance
+#define MAX_MSG_SIZE 4096 // Maximum message size
+
 int main(int argc, char *argv[]) {
-	
-	MPI_Init(&argc, &argv);
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	int tasks;
-	MPI_Comm_size(MPI_COMM_WORLD, &tasks);
-	
-	int left_neighbor_rank = (rank + 1) % tasks;
-	int right_neighbor_rank = (rank - 1) % tasks;
+    MPI_Init(&argc, &argv);
+    int rank, tasks;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &tasks);
 
-	int msg_size;
-	char *message;
-	MPI_Status status;
-	msg_size = 1 << 1;
-	message = (char *)malloc(msg_size);
-	printf("Message size %d\n", msg_size);
-	for (j = 1; j < 10; j++) {
-		if (j % 2 == 0) {
-			MPI_Send(message, msg_size, MPI_CHAR, right_neighbor_rank, 0, MPI_COMM_WORLD);	
-			printf("%d sends to %d and receives from %d\n", rank, right_neighbor_rank, left_neighbor_rank);
-		} else {
-			MPI_Recv(message, msg_size, MPI_CHAR, right_neighbor_rank, 0, MPI_COMM_WORLD, &status);
-			printf("%d sends to %d and receives from %d\n", rank, left_neighbor_rank, right_neighbor_rank);
-		}
-	}
-	free(message);
-	
-	MPI_Finalize();
+    // Determine neighbors in the ring
+    int right_neighbor_rank = (rank + 1) % tasks;
+    int left_neighbor_rank = rank - 1;
+    if (left_neighbor_rank < 0) left_neighbor_rank = tasks - 1;
 
+    for (int msg_size = 2; msg_size <= MAX_MSG_SIZE; msg_size *= 2) {
+        char *send_message = (char *)malloc(msg_size);
+        char *recv_message = (char *)malloc(msg_size);
+
+        if (!send_message || !recv_message) {
+            fprintf(stderr, "Memory allocation failed for message size %d bytes.\n", msg_size);
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+
+        double start_time = MPI_Wtime();
+
+        for (int i = 0; i < ITER; i++) {
+            // Perform the send and receive operations
+            MPI_Sendrecv(send_message, msg_size, MPI_CHAR, right_neighbor_rank, 0,
+                         recv_message, msg_size, MPI_CHAR, left_neighbor_rank, 0,
+                         MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+            // Here, you can manipulate the received data if needed
+        }
+
+        double end_time = MPI_Wtime();
+
+        if (rank == 0) {
+            double elapsed_time = end_time - start_time;
+            printf("Message size: %d bytes, Total time for %d iterations: %.3f seconds\n", msg_size, ITER, elapsed_time);
+        }
+
+        free(send_message);
+        free(recv_message);
+    }
+
+    MPI_Finalize();
+    return 0;
 }
