64253870ab2715cde42d79367dfdc254985ba93e
Xiaotian <156108153+xiaothua@users.noreply.github.com>
2024-03-07 12:51:32 -0500

    add on-node plots

--- plotting-pingpong.m
+++ plotting-pingpong.m
@@ -1,28 +1,43 @@
 clear all;
 clc;
 
-filename = '/Users/shelton/Desktop/nbpp.csv';
+filename = '/Users/shelton/Downloads/nbpp.csv';
+filename2 = '/Users/shelton/Downloads/nbpp-1.csv';
 data = readtable(filename);
+data2 = readtable(filename2);
 
 Message_Size = data{1:end, 1};
-Time = data{1:end, 2};
-Bandwidth = data{1:end, 3};
-Latency = data{1:end, 4};
+Time_offnode = data{1:end, 2};
+Bandwidth_offnode = data{1:end, 3};
+Latency_offnode = data{1:end, 4};
+
+Time_onnode = data2{1:end, 2};
+Bandwidth_onnode = data2{1:end, 3};
+Latency_onnode = data2{1:end, 4};
+
 
 % Bandwidth plot
 figure;
-semilogx(Message_Size, Bandwidth, "LineWidth", 2, "Marker", "o");
+semilogx(Message_Size, Bandwidth_offnode, "LineWidth", 2, "Marker", "o");
+hold on;
+semilogx(Message_Size, Bandwidth_onnode, "LineWidth", 2, "Marker", "o");
+hold off;
 xlabel('Message Size (bytes)');
 ylabel('Bandwidth (Megabytes/s)');
 title('Non-Block Ping-Pong, Bandwidth');
+legend("Off Node","On Node")
 grid on;
 set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on');
 
 % Latency plot
 figure;
-semilogx(Message_Size, Latency, "LineWidth", 2, "Marker", "o");
+semilogx(Message_Size, Latency_offnode, "LineWidth", 2, "Marker", "o");
+hold on;
+semilogx(Message_Size, Latency_onnode, "LineWidth", 2, "Marker", "o");
+hold off;
 xlabel('Message Size (bytes)');
 ylabel('Latency (us)');
 title('Non-Block Ping-Pong, Latency');
+legend("Off Node","On Node")
 grid on;
 set(gca, 'XMinorGrid', 'on', 'YMinorGrid', 'on');
